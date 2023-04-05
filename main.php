d63ade787c70698a0ddb3475f7d13a3490b18304
Lin Zeng <42827022+komaki0147@users.noreply.github.com>
2023-04-05 20:29:43 +0900

    Update main.php

--- main.php
+++ main.php
@@ -1,29 +1,7 @@
-
 <?php
-$num_1 = $_POST['num_1'];
-$num_2 = $_POST['num_2'];
-$cal = $_POST['cal'];
-$num_3 = 0;
-switch ($cal)
-{
-    case 'plus':
-        $num_3 = $num_1+$num_2;
-        break;
-    case 'sub':
-        $num_3 = $num_1-$num_2;
-        break;
-    case 'mul':
-        $num_3 = $num_1*$num_2;
-        break;
-    case 'div':
-        $num_3 = $num_1/$num_2;
-        break;
-}
-
-
-echo $num_3;
-
-
+		if (isset($_POST['submit'])) {
+			$names = explode("\n", $_POST['names']); // 将输入的名单分割成数组
+			$winner = array_rand($names); // 随机从数组中选择一个获奖者
+			echo "<p>恭喜获奖者：".trim($names[$winner])."！</p>"; // 显示获奖者
+		}
 ?>
-
-
